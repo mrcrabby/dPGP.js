f1940bf2905a4853c7049a6fa87a09c30e3bf2e4
Paul Sawaya <me@paulsawaya.com>
2010-04-05 17:38:40 -0400

    pretty auto updating chart

--- Try GP/gp_worker.js
+++ Try GP/gp_worker.js
@@ -1,59 +1,59 @@
-// importScripts('push.js','pushgp.js','gp_controller.js');
-// 
-// 
-// var pushGP;
-// 
-// GPWorker = function () {
-// 	this.gens = 0;
-// };
-// 
-// GPWorker.prototype.startGPWorker = function(numGens) {
-// 	
-//     // var numGens = message.data;
-// 
-//     var cases = [];
-// 
-//     var rawData = [ 2,4,6,8,10,12,14,16,18,20];
-//     
-//     for (var caseX = -10; caseX < 10; caseX++) {
-//         cases.push([caseX,/*rawData[caseX]*/ /*Math.pow(caseX,2) + 5*caseX*/ (2*(caseX+1))+2]);
-//     }
-//     
-//     workerDebug(cases);
-//     
-//     if (cases.length == 0) {
-//         alert("Please draw a function first.");
-//         return;
-//     }
-// 
-//     pushGP = new PushGP();
-//     pushGP.startGP(cases);/*[[0,1],[2,5],[3,100]]);*/        
-//     for (this.gens = 0; this.gens<numGens; this.gens++) {				
-// 		var str = pushGP.getProgramsHTMLString();
-// 			
-// 		postMessage({programsStr : str, bestFitness : pushGP.currentBestFitness});
-//             
-// 
-//         pushGP.doGeneration();            
-// 
-//     }
-// };
-// 
+importScripts('push.js','pushgp.js','gp_controller.js');
+
+
+var pushGP;
+
+GPWorker = function () {
+	this.gens = 0;
+};
+
+GPWorker.prototype.startGPWorker = function(numGens) {
+	
+    // var numGens = message.data;
+
+    var cases = [];
+
+    var rawData = [ 2,4,6,8,10,12,14,16,18,20];
+    
+    for (var caseX = -10; caseX < 10; caseX++) {
+        cases.push([caseX,/*rawData[caseX]*/ /*Math.pow(caseX,2) + 5*caseX*/ (2*(caseX+1))+2]);
+    }
+    
+    workerDebug(cases);
+    
+    if (cases.length == 0) {
+        alert("Please draw a function first.");
+        return;
+    }
+
+    pushGP = new PushGP();
+    pushGP.startGP(cases);/*[[0,1],[2,5],[3,100]]);*/        
+    for (this.gens = 0; this.gens<numGens; this.gens++) {				
+		var str = pushGP.getProgramsHTMLString();
+			
+		postMessage({programsStr : str, bestFitness : pushGP.currentBestFitness});
+            
+
+        pushGP.doGeneration();            
+
+    }
+};
+
 onmessage = function(message) {
-	// var worker = new GPWorker();
-	// 
-	// worker.startGPWorker(message.data);
+	var worker = new GPWorker();
+	
+	worker.startGPWorker(message.data);
 
 }
-// 
-// function updateStatus() {
-//     //do nothing.
-// }
-// 
-// function alert(str) {
-//     //no alerts inside of a thread, obviously.
-// }
-// 
-// function workerDebug(str) {
-//     postMessage ({'msgtype' : 'debug', 'msg' : str});
-// }
+
+function updateStatus() {
+    //do nothing.
+}
+
+function alert(str) {
+    //no alerts inside of a thread, obviously.
+}
+
+function workerDebug(str) {
+    postMessage ({'msgtype' : 'debug', 'msg' : str});
+}
